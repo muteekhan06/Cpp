d36e98bd32063818e13a1887638bae0ccce2b99c
Mutee ur Rehman <97782128+muteekhan06@users.noreply.github.com>
2023-10-22 20:45:40 +0500

    Add files via upload

---
+++ 01_Fabonacci.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main() {
+    // Print the Fibonacci series up to a given number of terms.
+    cout << "Enter number of terms: ";
+    int terms;
+    cin >> terms;
+    int first =0;
+    int second =1;
+    for (int i=0;i<terms;i++){
+        while (first<terms){
+        cout << first << "   ";
+        int next = first +second;
+        first = second;
+        second = next;
+        }
+    }
+}
